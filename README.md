579fea0c3525ccdc682ad8856dbe42fb6782d63e
Artyom Bogomolov <awesomemdl95@gmail.com>
2020-08-29 21:10:58 +0300

    Различные фиксы
    Баг при просмотре на большом мониторе
    Добавлен use strict и исправлены ошибки в js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Text-convert
+
+## Запуск npm start
+
+### Без поддержки ie 11
  
 https://mdl95-showcase.tk/textTransform/index.html
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,31 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
     const btn_before = document.querySelector('.js-before');
     const btn_after = document.querySelector('.js-after');
     const form = document.querySelector('.js-form');
     const resetAll = document.querySelector('.js-reset');
     const copyBtn = document.querySelector('.js-btn-copy');
     const copyThis = document.querySelector('.js-copy-this');
-   
 
-    btn_before.addEventListener('click', function(){
+    let textarea_beforeValue;
+    let textarea_beforeUpperCase;
+    let textarea_afterLowerCase;
+
+
+    btn_before.addEventListener('click', function () {
         const textarea_after = document.querySelector('.js-textarea-after');
         const textarea_before = document.querySelector('.js-textarea-before');
-        
+
         if (textarea_before.value == '' || textarea_before.value == null) {
             alert('Введите слово!')
         } else {
             textarea_beforeValue = textarea_before.value
-        
+
             textarea_beforeUpperCase = textarea_beforeValue.toUpperCase();
-    
+
             textarea_after.value = textarea_beforeUpperCase
         }
     });
 
-    btn_after.addEventListener('click', function(){
+    btn_after.addEventListener('click', function () {
         const textarea_after = document.querySelector('.js-textarea-after');
         const textarea_before = document.querySelector('.js-textarea-before');
-        
+
         if (textarea_before.value == '' || textarea_before.value == null) {
             alert('Введите слово!')
         } else {
@@ -34,16 +40,16 @@ document.addEventListener("DOMContentLoaded", function() {
             textarea_afterLowerCase = textarea_beforeValue.toLowerCase();
 
             const firstUpperCase = textarea_afterLowerCase[0].toUpperCase() + textarea_afterLowerCase.slice(1);
-            
+
             textarea_after.value = firstUpperCase
         }
     });
 
-    resetAll.addEventListener('click', function(){
+    resetAll.addEventListener('click', function () {
         form.reset();
     });
 
-    copyBtn.addEventListener('click', function(e) {
+    copyBtn.addEventListener('click', function (e) {
         const textarea_after = document.querySelector('.js-textarea-after');
         e.preventDefault();
         if (textarea_after.value == '' || textarea_after.value == null) {
@@ -55,12 +61,12 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    copyBtn.addEventListener('mouseover', function() {
+    copyBtn.addEventListener('mouseover', function () {
         copyThis.classList.add('active')
     })
 
-    copyBtn.addEventListener('mouseout', function() {
-        setTimeout(function() {
+    copyBtn.addEventListener('mouseout', function () {
+        setTimeout(function () {
             copyThis.classList.remove('active');
             copyThis.innerText = 'Copy this!'
         }, 1000)
